90f2ab4ee237eae7af5ccd5f80c1c0ae7b4a73e0
Luiz Otávio <luizotavio.03@outlook.com>
2023-10-01 12:52:41 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 
-const personagens = document.querySelectorAll(".personagem")
+const personagens = document.querySelectorAll(".personagem");
 
 botoes.forEach((botao, indice) => {
     botao.addEventListener("click", () => {
@@ -10,7 +10,7 @@ botoes.forEach((botao, indice) => {
         botao.classList.add("selecionado");
         personagens[indice].classList.add("selecionado")
     })
-})
+});
 
 function desselecionarPersonagem() {
     const personagemSelecionado = document.querySelector(".personagem.selecionado");
